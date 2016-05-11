c2ceed3fdd9d89972d782021413cd90a7083df78
Khawaja Samran <moon_samran@hotmail.com>
2016-05-11 18:39:15 +0200

    Update Topo=MPTCP_single_switch.py

--- Topo=MPTCP_single_switch.py
+++ Topo=MPTCP_single_switch.py
@@ -1,11 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf460
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
 
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
 #!/usr/bin/python\
 \
 """ Script to use MPTCP in Mininet. Run on commandline as, python singleswitch-mptcp.py\
